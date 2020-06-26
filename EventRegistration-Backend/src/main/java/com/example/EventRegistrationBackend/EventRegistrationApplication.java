874a8f0573e6192b32e1663f9365ebdf10adcc1b
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-26 14:42:18 -0400

    Update EventRegistrationApplication.java

--- EventRegistration-Backend/src/main/java/com/example/EventRegistrationBackend/EventRegistrationApplication.java
+++ EventRegistration-Backend/src/main/java/com/example/EventRegistrationBackend/EventRegistrationApplication.java
@@ -1,5 +1,5 @@
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @SpringBootApplication
 public class EventRegistrationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventRegistrationApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EventRegistrationApplication.class, args);
+  }
 
-	@RequestMapping("/")
-  	public String greeting(){
-    	return "Hello world!";
+  @RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
   }
 
 }
