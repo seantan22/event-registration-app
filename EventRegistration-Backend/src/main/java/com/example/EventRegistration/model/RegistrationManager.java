484e3b632015e988224f9dd2da93f31aeb3c3a88
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-27 11:19:14 -0400

    Added RegistrationManager class

---
+++ EventRegistration-Backend/src/main/java/com/example/EventRegistration/model/RegistrationManager.java
@@ -0,0 +1,60 @@
+package com.example.EventRegistration.model;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class RegistrationManager {
+	
+//	private int id;
+//	
+//	@Id
+//	public int getId() {
+//		return this.id;
+//	}
+//	
+//	public void setInt(int value) {
+//		this.id = value;
+//	}
+	
+	private Set<Person> persons;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	public Set<Person> getPersons() {
+		return this.persons;
+	}
+	
+	public void setPersons(Set<Person> personss) {
+		this.persons = personss;
+	}
+	
+	
+	private Set<Event> events;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	public Set<Event> getEvents() {
+		return this.events;
+	}
+	
+	public void setEvents(Set<Event> eventss) {
+		this.events = eventss;
+	}
+	
+	
+	private Set<CreditCard> creditCard;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	public Set<CreditCard> getCreditCard() {
+		return this.creditCard;
+	}
+	
+	public void setCreditCard(Set<CreditCard> creditCard) {
+		this.creditCard = creditCard;
+	}
+	
+}
