c38d93b93a6570a006a45b01568f7deb9d884f9e
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-29 07:18:28 -0400

    First test working

--- EventRegistration-Backend/src/test/java/com/example/EventRegistrationBackend/EventRegistrationApplicationTests.java
+++ EventRegistration-Backend/src/test/java/com/example/EventRegistrationBackend/EventRegistrationApplicationTests.java
@@ -1,8 +1,13 @@
 package com.example.EventRegistrationBackend;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = EventRegistrationApplicationTests.class)
 @SpringBootTest
 class EventRegistrationApplicationTests {
 
