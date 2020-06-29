9828e2d5cabddb6b8df1e008702f9f62e74bfd16
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-29 11:19:15 -0400

    Added Organizer service methods

--- EventRegistration-Backend/src/main/java/com/example/EventRegistration/service/EventRegistrationService.java
+++ EventRegistration-Backend/src/main/java/com/example/EventRegistration/service/EventRegistrationService.java
@@ -1,14 +1,20 @@
 package com.example.EventRegistration.service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.EventRegistration.dao.EventRepository;
+import com.example.EventRegistration.dao.OrganizerRepository;
 import com.example.EventRegistration.dao.PersonRepository;
+import com.example.EventRegistration.model.Event;
+import com.example.EventRegistration.model.Organizer;
 import com.example.EventRegistration.model.Person;
 
 @Service
@@ -17,12 +23,20 @@ public class EventRegistrationService {
 	@Autowired
 	private PersonRepository personRepository;
 	
+	@Autowired
+	private OrganizerRepository organizerRepository;
+	
+	@Autowired
+	private EventRepository eventRepository;
+	
+	
+	
 	/*** PERSON ***/
 	
 	@Transactional
 	public Person createPerson(String name) {
 		
-		if(name == null || name.trim().length() == 0 || name.replaceAll("//s", "").length() == 0 || name =="" ) {
+		if(name == null || name.trim().length() == 0 || name.replaceAll("//s", "").length() == 0 || name == "" ) {
 			throw new IllegalArgumentException("Person name cannot be empty");
 		} else if(personRepository.existsById(name)) {
 			throw new IllegalArgumentException("Person already exists");
@@ -38,9 +52,9 @@ public class EventRegistrationService {
 	@Transactional
 	public Person getPerson(String name) {
 		
-		if(name == null || name.trim().length() == 0 || name.replaceAll("//s", "").length() == 0 || name =="" ) {
+		if(name == null || name.trim().length() == 0 || name.replaceAll("//s", "").length() == 0 || name == "" ) {
 			throw new IllegalArgumentException("Person name cannot be empty");
-		} else if (!personRepository.existsById(name)) {
+		} else if(!personRepository.existsById(name)) {
 			throw new IllegalArgumentException("Person does not exist");
 		}
 		
@@ -57,9 +71,9 @@ public class EventRegistrationService {
 	@Transactional
 	public void deletePerson(String name) {
 		
-		if(name == null || name.trim().length() == 0 || name.replaceAll("//s", "").length() == 0 || name =="" ) {
+		if(name == null || name.trim().length() == 0 || name.replaceAll("//s", "").length() == 0 || name == "" ) {
 			throw new IllegalArgumentException("Person name cannot be empty");
-		} else if (!personRepository.existsById(name)) {
+		} else if(!personRepository.existsById(name)) {
 			throw new IllegalArgumentException("Person does not exist");
 		}
 		
@@ -68,16 +82,101 @@ public class EventRegistrationService {
 		
 	}
 	
+	
 	/*** ORGANIZER ***/
 	
+	@Transactional
+	public Organizer createOrganizer(String name) {
+		
+		if(name == null || name.trim().length() == 0 || name.replaceAll("//s", "").length() == 0 || name == "" ) {
+			throw new IllegalArgumentException("Organizer name cannot be empty");
+		} else if(organizerRepository.existsById(name)) {
+			throw new IllegalArgumentException("Organizer already exists");
+		}
+		
+		Organizer organizer = new Organizer();
+		organizer.setName(name);
+		organizerRepository.save(organizer);
+		return organizer;
+		
+	}
+	
+	@Transactional
+	public Person getOrganizer(String name) {
+		
+		if(name == null || name.trim().length() == 0 || name.replaceAll("//s", "").length() == 0 || name == "" ) {
+			throw new IllegalArgumentException("Organizer name cannot be empty");
+		} else if(!organizerRepository.existsById(name)) {
+			throw new IllegalArgumentException("Organizer does not exist");
+		}
+		
+		Person organizer = organizerRepository.findByName(name);
+		return organizer;
+		
+	}
+	
+	@Transactional
+	public List<Organizer> getAllOrganizers() {
+		return toList(organizerRepository.findAll());
+	}
+	
+	@Transactional
+	public void deleteOrganizer(String name) {
+		
+		if(name == null || name.trim().length() == 0 || name.replaceAll("//s", "").length() == 0 || name == "" ) {
+			throw new IllegalArgumentException("Organizer name cannot be empty");
+		} else if(!organizerRepository.existsById(name)) {
+			throw new IllegalArgumentException("Organizer does not exist");
+		}
+		
+		Organizer organizer = organizerRepository.findByName(name);
+		organizerRepository.delete(organizer);
+		
+	}
+	
+	@Transactional
+	public Organizer organizesEvent(Organizer organizer, Event event) {
+		
+		if(organizer == null) {
+			throw new IllegalArgumentException("Organizer must be selected");
+		} else if(!organizerRepository.existsById(organizer.getName())) {
+			throw new IllegalArgumentException("Organizer does not exist");
+		}
+		if(event == null) {
+			throw new IllegalArgumentException("Event must be selected");
+		} else if(!eventRepository.existsById(event.getName())) {
+			throw new IllegalArgumentException("Event does not exist");
+		}
+		
+		HashSet<Event> eventsOrganized = new HashSet<Event>();
+		Set<Event> organizedEvents = organizer.getOrganizes();
+		
+		if(organizedEvents != null) {
+			for(Event newEvent : organizedEvents) {
+				if(newEvent == event) {
+					throw new IllegalArgumentException("Organizer is already organizing event");
+				}
+				eventsOrganized.add(newEvent);
+			}
+		}
+		
+		eventsOrganized.add(event);
+		organizer.setOrganizes(eventsOrganized);
+		organizerRepository.save(organizer);
+		return organizer;
+		
+	}
 	
 	/*** EVENT ***/
+	
 	
 	
 	/*** REGISTRATION ***/
 	
 	
+	
 	/*** CREDIT CARD ***/
+	
 	
 	
 	/*** HELPER METHODS ***/
