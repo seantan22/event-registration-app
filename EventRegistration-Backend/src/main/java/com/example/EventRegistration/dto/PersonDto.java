92d9d8290bf8d3346cce9d9f8c255fb61c614f34
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-30 13:15:49 -0400

    Updated Person DTO

--- EventRegistration-Backend/src/main/java/com/example/EventRegistration/dto/PersonDto.java
+++ EventRegistration-Backend/src/main/java/com/example/EventRegistration/dto/PersonDto.java
@@ -1,5 +1,35 @@
 package com.example.EventRegistration.dto;
 
+import java.util.Collections;
+import java.util.List;
+
 public class PersonDto {
 
+	private String name;
+	private List<EventDto> eventsAttended;
+
+	public PersonDto() {
+	}
+
+	@SuppressWarnings("unchecked")
+	public PersonDto(String name) {
+		this(name, Collections.EMPTY_LIST);
+	}
+
+	public PersonDto(String name, List<EventDto> events) {
+		this.name = name;
+		this.eventsAttended = events;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public List<EventDto> getEventsAttended() {
+		return eventsAttended;
+	}
+
+	public void setEventsAttended(List<EventDto> events) {
+		this.eventsAttended = events;
+	}
 }
