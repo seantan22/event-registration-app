c52c890648ec40e3061ae9bb6b6886d536f5e8bd
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-30 13:15:55 -0400

    Updated Organizer DTO

--- EventRegistration-Backend/src/main/java/com/example/EventRegistration/dto/OrganizerDto.java
+++ EventRegistration-Backend/src/main/java/com/example/EventRegistration/dto/OrganizerDto.java
@@ -1,5 +1,37 @@
 package com.example.EventRegistration.dto;
 
-public class OrganizerDto {
+import java.util.Collections;
+import java.util.List;
 
+public class OrganizerDto {
+	
+	private String name;
+	private List<EventDto> organizedEvents;
+	
+	public OrganizerDto() {
+	}
+	
+	@SuppressWarnings("unchecked")
+	public OrganizerDto(String name) {
+		this.name = name;
+		this.organizedEvents = Collections.EMPTY_LIST;
+	}
+	
+	public OrganizerDto(String name, List<EventDto> events) {
+		this.name = name;
+		this.organizedEvents = events;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public List<EventDto> getOrganizedEvents() {
+		return organizedEvents;
+	}
+
+	public void setOrganizedEvents(List<EventDto> organizedEvents) {
+		this.organizedEvents = organizedEvents;
+	}
+	
 }
