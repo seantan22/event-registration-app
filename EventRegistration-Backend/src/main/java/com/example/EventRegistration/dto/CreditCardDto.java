984601977329c36c13fa41e2100ad3416ac4acba
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-30 13:16:11 -0400

    Updated Credit Card DTO

--- EventRegistration-Backend/src/main/java/com/example/EventRegistration/dto/CreditCardDto.java
+++ EventRegistration-Backend/src/main/java/com/example/EventRegistration/dto/CreditCardDto.java
@@ -1,5 +1,32 @@
 package com.example.EventRegistration.dto;
 
 public class CreditCardDto {
-
+	
+	private String accountNumber;
+	private int amount;
+	
+	public CreditCardDto() {
+	}
+	
+	public CreditCardDto(String accountNumber, int amount) {
+		this.accountNumber = accountNumber;
+		this.amount = amount;
+	}
+	
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+	public int getAmount() {
+		return amount;
+	}
+	
 }
