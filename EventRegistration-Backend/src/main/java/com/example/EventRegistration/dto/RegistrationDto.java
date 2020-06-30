abcce504ec7b7a44ae17e708f0f073b004f47ee1
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-30 13:16:03 -0400

    Updated Registration DTO

--- EventRegistration-Backend/src/main/java/com/example/EventRegistration/dto/RegistrationDto.java
+++ EventRegistration-Backend/src/main/java/com/example/EventRegistration/dto/RegistrationDto.java
@@ -1,5 +1,43 @@
 package com.example.EventRegistration.dto;
 
+import com.example.EventRegistration.model.CreditCard;
+
 public class RegistrationDto {
+
+	private PersonDto person;
+	private EventDto event;
+	private CreditCard creditCard;
+
+	public RegistrationDto() {
+	}
+
+	public RegistrationDto(PersonDto person, EventDto event) {
+		this.person = person;
+		this.event = event;
+	}
+
+	public EventDto getEvent() {
+		return event;
+	}
+
+	public void setEvent(EventDto event) {
+		this.event = event;
+	}
+
+	public PersonDto getPerson() {
+		return person;
+	}
+
+	public void setPerson(PersonDto person) {
+		this.person = person;
+	}
+	
+	public void setCreditCard(CreditCard creditCard) {
+		this.creditCard = creditCard;
+	}
+
+	public CreditCard getCreditCard() {
+		return creditCard;
+	}
 
 }
