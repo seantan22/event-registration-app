3275c6341673d1a68cb7f5f2e5b560cfa0f7cfe6
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-30 13:07:26 -0400

    Updated Event DTO

--- EventRegistration-Backend/src/main/java/com/example/EventRegistration/dto/EventDto.java
+++ EventRegistration-Backend/src/main/java/com/example/EventRegistration/dto/EventDto.java
@@ -1,5 +1,63 @@
 package com.example.EventRegistration.dto;
 
-public class EventDto {
+import java.sql.Time;
+import java.sql.Date;
 
+public class EventDto {
+	
+	private String name;
+	private Date date;
+	private Time startTime;
+	private Time endTime;
+	private String description;
+	private CreditCardDto creditCardDto;
+	
+	public EventDto() {
+		
+	}
+	
+	public EventDto(String name) {
+		this.name = name;
+		this.date = Date.valueOf("1971-01-01");
+		this.startTime = Time.valueOf("00:00:00");
+		this.endTime = Time.valueOf("23:59:59");
+		this.description = "--";
+	}
+	
+	public EventDto(String name, Date date, Time startTime, Time endTime, String description) {
+		this.name = name;
+		this.date = date;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.description = description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public Time getStartTime() {
+		return startTime;
+	}
+
+	public Time getEndTime() {
+		return endTime;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public CreditCardDto getCreditCard(){
+		return creditCardDto;
+	}
+	
+	public void setCreditCard(CreditCardDto creditCardDto) {
+		this.creditCardDto = creditCardDto;
+	}
+	
 }
