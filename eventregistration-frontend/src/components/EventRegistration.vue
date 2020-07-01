14be4083859dcbdbc76630d56e1d04101f392f1b
Sean Tan <sean.tan@mail.mcgill.ca>
2020-06-30 20:15:19 -0400

    Update EventRegistration.vue

--- eventregistration-frontend/src/components/EventRegistration.vue
+++ eventregistration-frontend/src/components/EventRegistration.vue
@@ -1,7 +1,6 @@
 <template>
     <div id="eventregistration">
         <h1> Event Registration App </h1>
-        <h3> People </h3>
         <table>
             <tr>
                 <td> Person Name </td>
